2ec5bb8348138646875b1c7015535b2b769ea9df
KARAN NANA KATAKDHOND <karankatakdhond23@gmail.com>
2026-04-07 23:21:41 +0530

    updated firebaseadmin

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -8,7 +8,9 @@ if (!admin.apps.length) {
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         // The replace() function ensures the private key formatting doesn't break
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+        : undefined,
       }),
     });
     console.log("Firebase Admin Initialized Successfully");
